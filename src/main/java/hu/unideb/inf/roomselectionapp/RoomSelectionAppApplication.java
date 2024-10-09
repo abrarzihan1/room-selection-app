d285bc8e4280bc1b69fb908a0c0b509cdb35c317
abrarzihan1 <abrarzihan2000@gmail.com>
2024-10-09 21:54:20 +0200

    Initial commit

---
+++ src/main/java/hu/unideb/inf/roomselectionapp/RoomSelectionAppApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.roomselectionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomSelectionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomSelectionAppApplication.class, args);
+    }
+
+}

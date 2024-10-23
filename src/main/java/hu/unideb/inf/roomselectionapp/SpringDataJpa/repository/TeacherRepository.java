267ce081e943575b84cf3c22503b994734a236b2
Anurag3703 <146084286+Anurag3703@users.noreply.github.com>
2024-10-23 22:07:05 +0200

    Updates 1.Started Testing Teacher Repository . 2.Added local database H2 databse for testing. 3.Initalized the test for findByName() method.

--- src/main/java/hu/unideb/inf/roomselectionapp/SpringDataJpa/repository/TeacherRepository.java
+++ src/main/java/hu/unideb/inf/roomselectionapp/SpringDataJpa/repository/TeacherRepository.java
@@ -3,6 +3,11 @@ package hu.unideb.inf.roomselectionapp.SpringDataJpa.repository;
 import hu.unideb.inf.roomselectionapp.SpringDataJpa.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.datatransfer.FlavorListener;
+import java.util.List;
+
 public interface TeacherRepository extends JpaRepository<Teacher,String >{
+
+    List<Teacher> findByName(String name);
 
 }
---
+++ src/test/java/hu/unideb/inf/roomselectionapp/repository/TeacherRepositoryTest.java
@@ -0,0 +1,43 @@
+package hu.unideb.inf.roomselectionapp.repository;
+
+import hu.unideb.inf.roomselectionapp.SpringDataJpa.model.Teacher;
+import hu.unideb.inf.roomselectionapp.SpringDataJpa.repository.TeacherRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+@DataJpaTest
+public class TeacherRepositoryTest {
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    Teacher teacher;
+
+
+    @BeforeEach
+    void setup(){
+        teacher =new Teacher("CS-001","Adamko",
+                "Adamko123@gmail.com","Computer Science");
+        teacherRepository.save(teacher);
+    }
+
+    @AfterEach
+    void tearDown(){
+        teacher = null;
+        teacherRepository.deleteAll();
+    }
+
+
+    @Test
+    void testByTeacherName(){
+        List<Teacher> teacherList = teacherRepository.findByName("Adamko");
+        assertThat(teacherList.get(0).getName()).isEqualTo(teacher.getName());
+    }
+
+}

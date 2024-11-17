c37bcf5ab2fc11d290efa6c7cde7190af9d47e3a
abrarzihan1 <abrarzihan2000@gmail.com>
2024-11-17 19:01:45 +0100

    Updated room

--- src/main/java/hu/unideb/inf/roomselectionapp/SpringDataJpa/model/Room.java
+++ src/main/java/hu/unideb/inf/roomselectionapp/SpringDataJpa/model/Room.java
@@ -1,9 +1,6 @@
 package hu.unideb.inf.roomselectionapp.SpringDataJpa.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "room")
 public class Room {
     @Id
     private String roomId;
--- src/main/java/hu/unideb/inf/roomselectionapp/SpringDataJpa/model/RoomType.java
+++ src/main/java/hu/unideb/inf/roomselectionapp/SpringDataJpa/model/RoomType.java
@@ -1,5 +1,11 @@
 package hu.unideb.inf.roomselectionapp.SpringDataJpa.model;
 
 public enum RoomType {
-    CONFERENCE, LABROOM, AUDITORIUM, COMPUTER_ROOM
+    LABROOM,
+    CONFERENCE,
+    AUDITORIUM,
+    CLASSROOM,
+    MEETING_ROOM,
+    LIBRARY
 }
+

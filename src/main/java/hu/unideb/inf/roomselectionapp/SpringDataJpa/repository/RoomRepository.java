7222e904c7ef59430a725bc1fef86f1b5aa371b3
Anurag3703 <146084286+Anurag3703@users.noreply.github.com>
2024-12-04 14:02:56 +0100

    Testing Room Repository

--- src/main/java/hu/unideb/inf/roomselectionapp/SpringDataJpa/repository/RoomRepository.java
+++ src/main/java/hu/unideb/inf/roomselectionapp/SpringDataJpa/repository/RoomRepository.java
@@ -1,11 +1,13 @@
 package hu.unideb.inf.roomselectionapp.SpringDataJpa.repository;
 
 import hu.unideb.inf.roomselectionapp.SpringDataJpa.model.Room;
+import hu.unideb.inf.roomselectionapp.SpringDataJpa.model.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, String> {
     List<Room> findByHasComputers(Boolean hasComputers);
+    List<Room> findByRoomType(RoomType roomType);
 
 }

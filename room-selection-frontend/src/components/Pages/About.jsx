14af4d6d912a29e2fa657fd1f3cc598c95b5f1f0
Anurag3703 <146084286+Anurag3703@users.noreply.github.com>
2024-12-09 00:15:09 +0100

    Added Photos

--- room-selection-frontend/src/components/Pages/About.jsx
+++ room-selection-frontend/src/components/Pages/About.jsx
@@ -4,7 +4,7 @@ import './About.css';
 const About = () => {
     const contributors = [
         { name: 'Marcio Tavares', role: 'Frontend Developer', image: '/Anurag.png' },
-        { name: 'Abrar Zihan', role: 'Backend Developer', image: '/cat.png' },
+        { name: 'Abrar Zihan', role: 'Backend Developer', image: '/Abrar.jpg' },
         { name: 'Anurag Tiwari', role: 'Database Developer', image: '/Anurag.png' },
         { name: 'Bishoy Mouris', role: 'Software Tester', image: 'bishoy.jpg' },
     ];
